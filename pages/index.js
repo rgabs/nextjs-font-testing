96415fa3baeccf88829a4abbd1a5380c0cf2537f
Rahul Gaba <rahulgaba16@gmail.com>
2021-02-08 20:52:45 +0530

    Optional with prerendering fonts

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,10 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Tangerine&display=block" />
+        <link rel="preload" href="https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZMprNA4A.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+
+
+
       </Head>
 
       <main>
@@ -64,6 +66,15 @@ export default function Home() {
       </footer>
 
       <style jsx>{`
+      /* latin */
+@font-face {
+font-family: 'Tangerine';
+font-style: normal;
+font-weight: 400;
+font-display: optional;
+src: url(https://fonts.gstatic.com/s/tangerine/v12/IurY6Y5j_oScZZow4VOxCZZMprNA4A.woff2) format('woff2');
+unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
 
 
         .container {
